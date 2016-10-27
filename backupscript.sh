623699eaa44d764967bb02afee74d6468714fec4
Vinh Ngu <root@v22016043565234083.happysrv.de>
2016-10-27 03:00:29 +0200

    2016-10-27:03:00:29

--- backupscript.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash 
-
-docker stop minecraft-server && \
-sleep 10 && \
-cd /root/Docker/Minecraft/ &&  \
-git add . && \
-git commit -m `date +%Y-%m-%d:%H:%M:%S` && \
-git push origin master && \
-docker start minecraft-server
