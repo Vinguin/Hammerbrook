4011315e2662a2502aa8906484e49077f4868e4c
Vinh Ngu <root@v22016043565234083.happysrv.de>
2016-10-27 02:52:47 +0200

    2016-10-27:02:52:47

--- backupscript.sh
+++ backupscript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-cd /root/Docker/Minecraft/ && \ 
-git add Hammerbrooklyn/ Hammerbrooklyn_the_end Hammerbrooklyn_nether && \
-git commit -m git commit -m `date +%Y-%m-%d:%H:%M:%S` && \
+cd /root/Docker/Minecraft/ &&  \
+git add . && \
+git commit -m `date +%Y-%m-%d:%H:%M:%S` && \
 git push origin master
